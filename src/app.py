b5cc35dbe500eba7726b2e1b6b02a6d83104183d
AndresLostaunau <39714785+AndresLostaunau@users.noreply.github.com>
2021-06-12 17:49:20 -0500

    css v1.0

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import psycopg2
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.routing import ValidationError
 
 app = Flask(__name__, static_url_path="/static")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/proyecto1dbp'
@@ -9,9 +10,9 @@ db = SQLAlchemy(app)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-class User(db.Model):
+class Client(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(32), nullable=False)
+    username = db.Column(db.String(32), unique=True, nullable=False)
     password = db.Column(db.String(32), nullable=False)
     balance = db.Column(db.Integer, nullable=False)
 
@@ -20,23 +21,29 @@ class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
     category = db.Column(db.String(32), nullable=False)
-    price = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float(precision=2), nullable=False)
+    image_url = db.Column(db.String(256), nullable=False)
 
 
 class Inventory(db.Model):
     g_id = db.Column(db.Integer, db.ForeignKey('game.id'), primary_key=True)
-    u_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    c_id = db.Column(db.Integer, db.ForeignKey('client.id'), primary_key=True)
 
 
 db.create_all()
 
 
-@app.route('/tiendas')
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+
+@app.route('/games')
 def print_tiendas():
     cursor = connection.cursor()
-    cursor.execute('SELECT * FROM tienda')
+    cursor.execute('SELECT * FROM game')
     data = cursor.fetchall()
-    return render_template('tienda.html', data=data)
+    return render_template('game.html', data=data)
 
 
 @app.route('/tiendas/<name>')
@@ -51,30 +58,39 @@ def print_stock(name):
     return render_template('stock.html', data=data)
 
 
-@app.route('/register')
-def registerUser():
+@app.route('/register', methods=['GET'])
+def registerUserGet():
     return render_template('register.html')
 
 
 @app.route('/register', methods=['POST'])
-def register():
-    content = request.json
-    print(request)
+def registerUserPost():
+    content = request.form
+    if len(content['username']) > 32:
+        raise ValidationError('Nombre del usuario es mayor al límite de 32 caracteres.')
+    elif len(content['password']) > 32:
+        raise ValidationError('Contraseña mayor del límite de 32 caracteres.')
+    elif content['psw'] is not content['psw-repeat']:
+        raise ValidationError('Contraseñas no coinciden.')
+    elif Client.query.filter_by(username=content['username']).first() is not None:
+        raise ValidationError('Nombre del usuario ya existe.')
+    db.session.add(Client(username=content['username'], password=content['psw'], balance=200))
+    return 'Otro usuario registrado'
 
 
-@app.route('/initialize')
+@app.route('/init')
 def initialize():
     cursor = connection.cursor()
     cursor.execute('INSERT INTO game (name,category,price)'
                    ' VALUES (\'Counter-Strike: Global Offensive\',\'First Person Shooter\',0);'
                    'INSERT INTO game (name,category,price)'
-                   ' VALUES (\'Dota 2\',\'MOBA\',10);'
+                   ' VALUES (\'Dota 2\',\'MOBA\',10.50);'
                    'INSERT INTO game (name,category,price)'
-                   ' VALUES (\'PLAYERUNKNOWN\'\'S BATTLEGROUNDS\',\'Battle Royale\',60);'
+                   ' VALUES (\'PLAYERUNKNOWN\'\'S BATTLEGROUNDS\',\'Battle Royale\',60.90);'
                    'INSERT INTO game (name,category,price)'
-                   ' VALUES (\'Apex Legends\',\'First Person Shooter\',20);'
+                   ' VALUES (\'Apex Legends\',\'First Person Shooter\',20.80);'
                    'INSERT INTO game (name,category,price)'
-                   ' VALUES (\'Grand Theft Auto V\',\'Open World\',100);'
+                   ' VALUES (\'Grand Theft Auto V\',\'Open World\',100.00);'
                    'COMMIT;')
     return "inicializado"
 
