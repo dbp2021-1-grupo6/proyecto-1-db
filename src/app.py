eddb0aaebe569ce61fa4b3779945e13e2667e2a5
AndresLostaunau <39714785+AndresLostaunau@users.noreply.github.com>
2021-06-11 21:40:49 -0500

    new dbmodel

--- src/app.py
+++ src/app.py
@@ -1,36 +1,31 @@
 import psycopg2
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/proyecto1dbp'
 connection = psycopg2.connect('host=localhost port=5432 user=postgres password=password dbname=proyecto1dbp')
 db = SQLAlchemy(app)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-class Usuario(db.Model):
-    username = db.Column(db.String(32), primary_key=True)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), nullable=False)
+    password = db.Column(db.String(32), nullable=False)
     balance = db.Column(db.Integer, nullable=False)
 
 
-class Juego(db.Model):
-    nombre = db.Column(db.String(64), primary_key=True)
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    category = db.Column(db.String(32), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
 
 
-class Tienda(db.Model):
-    nombre = db.Column(db.String(32), primary_key=True)
-
-
-class Inventario(db.Model):
-    j_nombre = db.Column(db.String(64), db.ForeignKey('juego.nombre'), primary_key=True)
-    u_nombre = db.Column(db.String(32), db.ForeignKey('usuario.username'), nullable=False)
-
-
-class Stock(db.Model):
-    j_nombre = db.Column(db.String(64), db.ForeignKey('juego.nombre'), primary_key=True)
-    t_nombre = db.Column(db.String(32), db.ForeignKey('tienda.nombre'), nullable=False)
-    costo = db.Column(db.Float(precision=2), nullable=False)
+class Inventory(db.Model):
+    g_id = db.Column(db.Integer, db.ForeignKey('game.id'), primary_key=True)
+    u_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
 
 
 db.create_all()
@@ -56,24 +51,30 @@ def print_stock(name):
     return render_template('stock.html', data=data)
 
 
+@app.route('/register')
+def registerUser():
+    return render_template('register.html')
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    content = request.json
+    print(request)
+
+
 @app.route('/initialize')
 def initialize():
     cursor = connection.cursor()
-    cursor.execute('INSERT INTO tienda (nombre) VALUES (\'steam\');'
-                   'INSERT INTO tienda (nombre) VALUES (\'epic_games\');'
-                   'INSERT INTO tienda (nombre) VALUES (\'battledotnet\');'
-                   'INSERT INTO juego (nombre) VALUES (\'Counter-Strike: Global Offensive\');'
-                   'INSERT INTO juego (nombre) VALUES (\'Dota 2\');'
-                   'INSERT INTO juego (nombre) VALUES (\'PLAYERUNKNOWN\'\'S BATTLEGROUNDS\');'
-                   'INSERT INTO juego (nombre) VALUES (\'Apex Legends\');'
-                   'INSERT INTO juego (nombre) VALUES (\'Grand Theft Auto V\');'
-                   'INSERT INTO stock (j_nombre,t_nombre,costo)'
-                   ' VALUES (\'Counter-Strike: Global Offensive\',\'steam\',0);'
-                   'INSERT INTO stock (j_nombre,t_nombre,costo) VALUES (\'Dota 2\',\'steam\',0);'
-                   'INSERT INTO stock (j_nombre,t_nombre,costo)'
-                   ' VALUES (\'PLAYERUNKNOWN\'\'S BATTLEGROUNDS\',\'steam\',52.00);'
-                   'INSERT INTO stock (j_nombre,t_nombre,costo) VALUES (\'Apex Legends\',\'steam\',0);'
-                   'INSERT INTO stock (j_nombre,t_nombre,costo) VALUES (\'Grand Theft Auto V\',\'steam\',49.17);'
+    cursor.execute('INSERT INTO game (name,category,price)'
+                   ' VALUES (\'Counter-Strike: Global Offensive\',\'First Person Shooter\',0);'
+                   'INSERT INTO game (name,category,price)'
+                   ' VALUES (\'Dota 2\',\'MOBA\',10);'
+                   'INSERT INTO game (name,category,price)'
+                   ' VALUES (\'PLAYERUNKNOWN\'\'S BATTLEGROUNDS\',\'Battle Royale\',60);'
+                   'INSERT INTO game (name,category,price)'
+                   ' VALUES (\'Apex Legends\',\'First Person Shooter\',20);'
+                   'INSERT INTO game (name,category,price)'
+                   ' VALUES (\'Grand Theft Auto V\',\'Open World\',100);'
                    'COMMIT;')
     return "inicializado"
 
